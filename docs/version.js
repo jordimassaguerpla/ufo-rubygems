6057fc621aa59e028b41d143ca854edfa7644004
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2018-03-31 01:16:16 +0200

    update text, add version and review design

---
+++ docs/version.js
@@ -0,0 +1,1 @@
+document.write("Sat Mar 31 00:50:46 CEST 2018")
--- update.sh
+++ update.sh
@@ -5,5 +5,7 @@ cd ruby-advisory-db
 git pull
 cd ..
 ruby get_stats_csv.rb > docs/stats.csv
+version=$(date)
+echo "document.write(\"$version\")" > docs/version.js
 
 
